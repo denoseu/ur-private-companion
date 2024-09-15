f97ce12c86cd11f8a3a07e62245bb166ceee7727
denoseu <denisefeliciatiowanni@gmail.com>
2024-09-15 16:50:43 +0700

    initial commit <3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function RadialCircle() {
+  return <div className='radial-circle'></div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <RadialCircle />
+      <h3>denise is here with you! :)</h3>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div className="text">
+        <h3>denise is here with you! :)</h3>
+    </div>
+  );
+};
+
+export default Main;
